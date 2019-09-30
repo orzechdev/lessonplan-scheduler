520dd6d92fa514c1b4e9b0b17750c746b1354652
orzechdev <tarasiukdawid@gmail.com>
2019-10-01 00:05:53 +0200

    fix short data type

--- backend/main.py
+++ backend/main.py
@@ -5,10 +5,10 @@ print("DOES IT PRINT ITSELF? - if yes, it works!")
 
 day_count = 5
 lesson_count = 8
-rooms = np.zeros(11, dtype=np.uint16)
-subjects = np.zeros(11, dtype=np.uint16)
-teachers = np.zeros(11, dtype=np.uint16)
-classes = np.zeros(11, dtype=np.uint16)
+rooms = np.zeros(11, dtype=np.ushort)
+subjects = np.zeros(11, dtype=np.ushort)
+teachers = np.zeros(11, dtype=np.ushort)
+classes = np.zeros(11, dtype=np.ushort)
 
 run_algorithm(
         day_count,
