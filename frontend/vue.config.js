19d831641f206b608c2948bb4d741cc9028a56f3
Dawid Tarasiuk <tarasiukdawid@gmail.com>
2019-11-08 20:16:54 +0100

    create frontend docker container

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,6 +2,10 @@ module.exports = {
   "transpileDependencies": [
     "vuetify"
   ],
+  chainWebpack: config => {
+    // remove vue-cli-service's progress output
+    config.plugins.delete('progress')
+  },
   devServer: {
     watchOptions: {
       poll: true
